0c78e7de427d1cdde19e6b82b814f7ad0448a626
sujansiva0504 <151783309+sujansiva0504@users.noreply.github.com>
2023-11-23 19:06:57 -0500

    Add files via upload

---
+++ Portfolio copy/src/Portfolio_main.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
+ */
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sujansiva
+ */
+public class Portfolio_main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        
+        
+        
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
