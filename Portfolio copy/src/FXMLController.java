02a151fe32c84b8c72b604b2e31d6ef4b613017d
sujansiva0504 <151783309+sujansiva0504@users.noreply.github.com>
2023-11-24 17:09:28 -0500

    Add files via upload

--- Portfolio copy/src/FXMLController.java
+++ Portfolio copy/src/FXMLController.java
@@ -4,26 +4,102 @@
  */
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.input.KeyEvent;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.AreaChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
  *
  * @author sujansiva
  */
-
 public class FXMLController implements Initializable {
-    private static Double totalEquity; //Everytime that the investor profile is updated. This should be set to the investor's equity via setOnAction for UpdateInvestorProfileButton
-    public static Double getTotalEquity(){
-        return totalEquity;
+
+    @FXML
+    private AnchorPane rootPane;
+
+    @FXML
+    private TextField sec1;
+
+    @FXML
+    private TextField sec2;
+
+    @FXML
+    private TextField sec3;
+
+    @FXML
+    private Label LabelName;
+
+    @FXML
+    private TextField TextFieldName;
+
+    @FXML
+    private Label LabelRisk;
+
+   
+
+    @FXML
+    private Label LabelAge;
+
+    @FXML
+    private TextField TextFieldAge;
+
+    @FXML
+    private Button Update;
+
+  
+
+    @FXML
+    private Label LabelAsset;
+
+    @FXML
+    private CheckBox CB1;
+
+    @FXML
+    private CheckBox CB2;
+
+    @FXML
+    private CheckBox CB3;
+
+    @FXML
+    private CheckBox CB4;
+
+    @FXML
+    private Button Refresh;
+
+    @FXML
+    private TextField BarSearch;
+
+    @FXML
+    private ListView<String> ListView;
+
+    @FXML
+    void ListviewPop(MouseEvent event) {
+    ListView.setOpacity(1.0);
     }
-    /**
-     * Initializes the controller class.
-     */
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+       
+
+}
 }
--- Portfolio copy/src/Portfolio_main.java
+++ Portfolio copy/src/Portfolio_main.java
@@ -3,33 +3,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
  */
 
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-
-/**
+/** "
  *
  * @author sujansiva
  */
 public class Portfolio_main extends Application {
     
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+   Parent root = FXMLLoader.load(getClass().getResource("FXML.fxml"));
+        Scene scene= new Scene(root);
         
-        
-        
-        
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-
         primaryStage.setScene(scene);
         primaryStage.show();
+   
     }
 
     /**
