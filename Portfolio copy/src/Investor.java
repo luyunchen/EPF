1815c293d1b0bee2e4d815f6f34f2dfb26a77980
Mustard Gaming <luyunchen04@gmail.com>
2023-11-23 20:32:23 -0500

    Added Investor class

---
+++ Portfolio copy/src/Investor.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author musta
+ */
+public class Investor {
+    private String name;
+    private double equity;
+    private String riskTolerance;
+    
+    public Investor(){};
+    
+    public Investor(String name, double equity, String riskTolerance){
+        this.name = name;
+        this.equity = equity;
+        this.riskTolerance = riskTolerance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getEquity() {
+        return equity;
+    }
+
+    public String getRiskTolerance() {
+        return riskTolerance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEquity(double equity) {
+        this.equity = equity;
+    }
+
+    public void setRiskTolerance(String riskTolerance) {
+        this.riskTolerance = riskTolerance;
+    }
+    
+    
+}
