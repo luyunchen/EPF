45a4f32f50cd33881ecfb60662fa0a797ea6b299
LuyunChen <luyunchen04@gmail.com>
2023-11-26 11:45:53 -0500

    Fixed weights.get issue

--- Portfolio copy/src/FXMLController.java
+++ Portfolio copy/src/FXMLController.java
@@ -81,7 +81,7 @@ public class FXMLController implements Initializable {
     
     
     
-    public static List<double[]> weights; //These are some random weights of the stocks for each portfolio. There's gonna be 20 portfolio, meaning 20 data points.
+    public static List<List<Double>> weights; //These are some random weights of the stocks for each portfolio. There's gonna be 20 portfolio, meaning 20 data points.
         
         
         
@@ -238,26 +238,27 @@ public class FXMLController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        weights.add(new double[]{0.25, 0.3, 0.35}); //Add random diversifications to the List of weights
-        weights.add(new double[]{0.1, 0.5, 0.4});
-        weights.add(new double[]{0.2, 0.1, 0.6});
-        weights.add(new double[]{0.3, 0.2, 0.4});
-        weights.add(new double[]{0.15, 0.4, 0.45});
-        weights.add(new double[]{0.4, 0.2, 0.35});
-        weights.add(new double[]{0.3, 0.4, 0.2});
-        weights.add(new double[]{0.2, 0.3, 0.4});
-        weights.add(new double[]{0.1, 0.3, 0.5});
-        weights.add(new double[]{0.35, 0.25, 0.4});
-        weights.add(new double[]{0.2, 0.25, 0.55});
-        weights.add(new double[]{0.3, 0.15, 0.55});
-        weights.add(new double[]{0.25, 0.35, 0.4});
-        weights.add(new double[]{0.15, 0.3, 0.55});
-        weights.add(new double[]{0.4, 0.1, 0.5});
-        weights.add(new double[]{0.25, 0.35, 0.4});
-        weights.add(new double[]{0.3, 0.1, 0.6});
-        weights.add(new double[]{0.1, 0.4, 0.5});
-        weights.add(new double[]{0.2, 0.5, 0.3});
-        weights.add(new double[]{0.3, 0.3, 0.35});
+         //Add random diversifications to the List of weights
+        weights.add(List.of(0.25, 0.3, 0.35));
+        weights.add(List.of(0.1, 0.5, 0.4));
+        weights.add(List.of(0.2, 0.1, 0.6));
+        weights.add(List.of(0.3, 0.2, 0.4));
+        weights.add(List.of(0.15, 0.4, 0.45));
+        weights.add(List.of(0.4, 0.2, 0.35));
+        weights.add(List.of(0.3, 0.4, 0.2));
+        weights.add(List.of(0.2, 0.3, 0.4));
+        weights.add(List.of(0.1, 0.3, 0.5));
+        weights.add(List.of(0.35, 0.25, 0.4));
+        weights.add(List.of(0.2, 0.25, 0.55));
+        weights.add(List.of(0.3, 0.15, 0.55));
+        weights.add(List.of(0.25, 0.35, 0.4));
+        weights.add(List.of(0.15, 0.3, 0.55));
+        weights.add(List.of(0.4, 0.1, 0.5));
+        weights.add(List.of(0.25, 0.35, 0.4));
+        weights.add(List.of(0.3, 0.1, 0.6));
+        weights.add(List.of(0.1, 0.4, 0.5));
+        weights.add(List.of(0.2, 0.5, 0.3));
+        weights.add(List.of(0.3, 0.3, 0.35));
 
 }
 }
